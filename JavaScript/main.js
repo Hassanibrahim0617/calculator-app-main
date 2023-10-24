f8f5359ea52933a5b3a3ca3b946be1df672ff81d
Ibrahim Hassan <hassan.ibb120044@gmail.com>
2023-10-24 13:42:53 +0100

    Add files via upload

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,7 +1,29 @@
 const resultCalc = document.getElementById('result');
-console.dir(resultCalc);
-let btnNumber = document.querySelectorAll('button');
+const btn = document.querySelectorAll('button');
+const operators = ['+', '-', '*', '/', '='];
 
-btnNumber.forEach(function(button){
-    console.dir(button);
+let result = '';
+
+// calculator btn
+
+btn.forEach(function (button) {
+   button.addEventListener('click', (e) => calculate(e.currentTarget.dataset.name));
 });
+
+function calculate(btnValues){
+   if(btnValues === '=' && btnValues !== ''){
+      result = eval(result);
+   }else if(btnValues === 'del'){
+      result = result.toString().slice(0, -1,);
+   }else if(btnValues === 'reset'){
+      result = ''
+   }else{
+      if(result === '' && operators.includes(btnValues)) return;
+         result += btnValues;
+   }
+
+   resultCalc.innerHTML = result;
+};
+
+
+
